4009aedc7b36e4e9b11550bae3d21d4cb773b88a
凪沙 阿部 <170070@jc-21.jp>
2019-12-23 16:33:25 +0900

    阿部　Confilm画面実装

--- team2_bento/src/team2_bento/ConfirmServlet.java
+++ team2_bento/src/team2_bento/ConfirmServlet.java
@@ -1,6 +1,12 @@
 package jp.ac.jc21.jk3a34;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -27,10 +33,41 @@ public class ConfirmServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd = request.getRequestDispatcher("/Confirm.jsp");
+		final String driverName = "oracle.jdbc.driver.OracleDriver";
+		final String url = "jdbc:oracle:thin:@192.168.54.192:1521/orcl";
+		final String id = "team2";   //
+		final String pass = "Oracle_11g";  //
+		
+		try {
+			Class.forName(driverName);
+			Connection connection=DriverManager.getConnection(url,id,pass);
+			PreparedStatement st = 
+					connection.prepareStatement(//ïŸìññºÅAå¬êîÅAã‡äzÇï\é¶
+						"select BENTO_NAME,SUM(QTY) as qtyg,sum(qty*price)as price from emp_order left outer join bento on(emp_order.bento_ID=bento.bento_ID) group by bento_name"
+						);
+			ResultSet rs = st.executeQuery();
+			ArrayList<String[]>list= new ArrayList<String[]>();
+			
+			while(rs.next() != false) {
+				String[] s = new String[3];
+				
+				s[0]=rs.getString("bento_name");
+				s[1]=rs.getString("qtyg");
+				s[2]=rs.getString("price");
+				list.add(s);
+				}
+			request.setAttribute("list", list);
+		RequestDispatcher rd = request.getRequestDispatcher("/Confilm.jsp");
 		rd.forward(request, response);
+		}catch(ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException");
+			e.printStackTrace(System.out);
+		}catch(SQLException e) {
+			System.out.println("SQLException");
+			e.printStackTrace(System.out);
+		}
 	}
 
 }
